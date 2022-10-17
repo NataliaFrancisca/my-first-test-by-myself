4265721a40083585c04b4094697497e883c9d29f
Natalia Francisca <nathifrancisca@gmail.com>
2022-10-17 16:13:24 -0300

    test: create tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,102 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('form elements must render corretly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+  const legendElement = screen.queryByText("Create Account");
+  expect(legendElement).toBeInTheDocument();
+
+  const labelNameElement = screen.queryByLabelText("Name:");
+  expect(labelNameElement).toBeInTheDocument();
+
+  const labelAgeElement = screen.queryByLabelText("Age:");
+  expect(labelAgeElement).toBeInTheDocument();
+
+  const labelCityElement = screen.queryByLabelText("City:");
+  expect(labelCityElement).toBeInTheDocument();
+
+  const buttonElement = screen.queryByText('Create');
+  expect(buttonElement).toBeInTheDocument();
+})
+
+test('inputs placeholder texts are render corretly', () => {
+  render(<App />);
+
+  const nameInputPlaceholder = screen.queryByPlaceholderText("type your name");
+  expect(nameInputPlaceholder).toBeInTheDocument();
+
+  const ageInputPlaceholder = screen.queryByPlaceholderText("type your age");
+  expect(ageInputPlaceholder).toBeInTheDocument();
+
+  const cityInputPlaceholder = screen.queryByPlaceholderText("type your city");
+  expect(cityInputPlaceholder).toBeInTheDocument();
+})
+
+test('after the button clicked, all the inputs values are get corretly', () => {
+  render(<App />);
+
+  const buttonSubmit = screen.queryByRole("button", {name: "Create"})
+  const nameInput = screen.queryByPlaceholderText("type your name");
+  const ageInput = screen.queryByPlaceholderText("type your age");
+  const cityInput = screen.queryByPlaceholderText("type your city");
+
+  fireEvent.change(nameInput, {
+    target: {
+      value: "Natalia"
+    }
+  })
+
+  fireEvent.change(ageInput, {
+    target: {
+      value: 22
+    }
+  })
+
+  fireEvent.change(cityInput, {
+    target: {
+      value: 'São Paulo'
+    }
+  })
+
+  fireEvent.click(buttonSubmit, new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true
+  }))
+
+  const nameInputTyped = screen.queryByText("Natalia");
+  const ageInputTyped = screen.queryByText(22);
+  const cityInputTyped = screen.queryByText("São Paulo");
+
+  expect(nameInputTyped).toHaveTextContent("Natalia"); 
+  expect(ageInputTyped).toHaveTextContent(22); 
+  expect(cityInputTyped).toHaveTextContent("São Paulo");
+})
+
+test('the message element shows corretly after the button was clicked', () => {
+  render(<App />);
+
+  const buttonSubmit = screen.queryByRole("button", {name: "Create"})
+
+  fireEvent.click(buttonSubmit, new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true
+  }))
+
+  const messageElement = screen.queryByRole("article");
+  expect(messageElement).toBeInTheDocument();
+})
+
+test('the error message element shows corretly after the validation', () => {
+  render(<App />);
+
+  const buttonSubmit = screen.queryByRole("button", {name: "Create"})
+
+  fireEvent.click(buttonSubmit, new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true
+  }))
+
+  const errorMessageElement = screen.queryByText("You need to fill all the inputs");
+  expect(errorMessageElement).toBeInTheDocument();
+})
