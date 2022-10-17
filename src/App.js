e4d1e7102d51f0dfdaf27f1846693af07c0fb0b7
Natalia Francisca <nathifrancisca@gmail.com>
2022-10-17 16:13:07 -0300

    feat: create project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [city, setCity] = useState();
+
+  const [showMessage, setShowMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  const onSetName = (value) => setName(value);
+  const onSetAge = (value) => setAge(value);
+  const onSetCity = (value) => setCity(value);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    // setShowMessage(true);
+    validateInputs();
+  }
+
+  const validateInputs = () => {
+    const validateName = !name || name.length < 2 ? false : true;
+    const validateAge = !age || age < 0 && age > 200 ? false : true;
+    const validateCity = !city || city.length < 2 ? false : true;
+
+    if(validateName && validateAge && validateCity){
+      setShowMessage(true);
+      setShowErrorMessage(false);
+    }else{
+      setShowMessage(false);
+      setShowErrorMessage(true);
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitForm}>
+        <legend>Create Account</legend>
+
+          {showErrorMessage && <p>You need to fill all the inputs</p>}
+
+          <label>Name:
+            <input type="text" name="name" placeholder="type your name"
+              onChange={(event) => onSetName(event.target.value)}
+            />
+          </label>
+
+          <label>Age:
+            <input type="number" name="age" placeholder="type your age"
+              onChange={(event) => onSetAge(event.target.value)}
+            />
+          </label>
+
+          <label>City:
+            <input type="text" name="city" placeholder="type your city"
+              onChange={(event) => onSetCity(event.target.value)}
+            />
+          </label>
+
+          <button type="submit">Create</button>
+      </form>
+
+      {showMessage && 
+        <article>
+          <h2>Olá, <span>{name}</span></h2>
+          <p>Você tem <span>{age}</span> anos e mora em <span>{city}</span></p>
+        </article>
+      }
     </div>
   );
 }
